7161a787c8bcf5044015f1df81bed0730a399e02
Arthur Felipe Pallu <arthurfelipepallu@gmail.com>
2024-08-02 13:25:21 -0300

    Land Page Cards lead to other page

--- src/Components/Cards/index.tsx
+++ src/Components/Cards/index.tsx
@@ -2,23 +2,30 @@ import "./styles.css";
 //import about_gif from "../../assets/imgs/bookwriting.gif"
 import { InfoCardDTO } from "../../assets/Models/Cards/InfoCard";
 import { getImageURL } from "../../assets/utils/image-utils";
+import { Link } from "react-router-dom";
 
+type Prop = {
+  infocardProp: InfoCardDTO;
+};
 
-type Prop={
-    infocardProp : InfoCardDTO;
-}
-
-
-
-export default function InfoCard({infocardProp}:Prop) {
-  return <>
-    <div className="apdw-infocard-div" 
-    style={{backgroundColor : infocardProp.bgcolor,
-            color:infocardProp.fontcolor  }}>
-        <img src={getImageURL(infocardProp.imgUrl)} alt="" 
-        />
+export default function InfoCard({ infocardProp }: Prop) {
+  return (
+    <>
+        <Link to="/games" className="apdw-link-style">
+      <div
+        className="apdw-infocard-div"
+        style={{
+          backgroundColor: infocardProp.bgcolor,
+          color: infocardProp.fontcolor,
+        }}
         
-        {infocardProp.name}
-    </div>
-  </>;
+      >
+          <img src={getImageURL(infocardProp.imgUrl)} alt="" />
+          
+          {infocardProp.name}
+        
+      </div>
+      </Link>
+    </>
+  );
 }
--- src/Components/Headers/index.tsx
+++ src/Components/Headers/index.tsx
@@ -8,10 +8,35 @@ export default function Header() {
     <header>
       <div className="apdw-home-header">
         <div className="apdw-home-header-inside">
-          <HeaderIcon />
-          {HeaderContactService.findAll().map((contact) => (
-            <HeaderContact key={contact.id} contactProp={contact} />
-          ))}
+          {/* <form id="catalogo-barra-pesquisa" className="barra-pesquisa">
+            <button
+              type="submit"
+              id="devcom-btn-submit"
+              className="devcom-btn-pesquisa"
+            >
+              {" "}
+              🔎︎{" "}
+            </button>
+            <input
+              type="text"
+              className="devcom-search-input"
+              placeholder="Nome do Produto"
+            />
+            <button
+              type="reset"
+              id="devcom-btn-reset"
+              className="devcom-btn-pesquisa"
+            >
+              {" "}
+              🗙{" "}
+            </button>
+          </form> */}
+          <div className="apdw-header-icons">
+            <HeaderIcon />
+            {HeaderContactService.findAll().map((contact) => (
+              <HeaderContact key={contact.id} contactProp={contact} />
+            ))}
+          </div>
         </div>
       </div>
     </header>
